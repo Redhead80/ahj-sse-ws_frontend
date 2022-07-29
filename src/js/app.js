fb289b24cb22459d7c2e4d58b6331938305e4a72
Redhead80 <g.utkin80@yandex.ru>
2022-07-30 00:05:03 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 import Widget from './Widget';
 
-const widget = new Widget('wss://evgeniy-27-ahj-sse-ws.herokuapp.com/ws');
+const widget = new Widget('wss://ahj-sse-ws-chat.herokuapp.com/ws');
 
 // const widget = new Widget('ws://localhost:8080/ws');
 
