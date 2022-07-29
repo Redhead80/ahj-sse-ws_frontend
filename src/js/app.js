6d4706a8bfa9141790acc16bbf66a395d8ff2ed0
Redhead80 <g.utkin80@yandex.ru>
2022-07-29 22:58:09 +0300

    commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+import Widget from './Widget';
+
+const widget = new Widget('wss://evgeniy-27-ahj-sse-ws.herokuapp.com/ws');
+
+// const widget = new Widget('ws://localhost:8080/ws');
+
+widget.createForm();
